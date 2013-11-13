753f9c67655f84f7e4cdc1fda0d7ae636459445d
Chris Bolton <cdbolton@uga.edu>
2013-11-13 17:07:25 -0500

    updating reservation

--- ReservationImpl.java
+++ ReservationImpl.java
@@ -3,7 +3,7 @@ package entity;
 import java.math.BigDecimal;
 import org.joda.time.DateTime;
 
-public class ReservationImpl implements Reservation {
+public class ReservationImpl implements Reservation{
 	private int id;
 	private String email;
 	private Vehicle rentedVehicle;
@@ -20,92 +20,51 @@ public class ReservationImpl implements Reservation {
 		this.price = price;
 	}
 
-	/* (non-Javadoc)
-	 * @see entity.Reservation#getId()
-	 */
-	@Override
 	public int getId() {
 		return id;
 	}
 
-	/* (non-Javadoc)
-	 * @see entity.Reservation#setId(int)
-	 */
-	@Override
+
 	public void setId(int id) {
 		this.id = id;
 	}
 
-	/* (non-Javadoc)
-	 * @see entity.Reservation#getEmail()
-	 */
-	@Override
 	public String getEmail() {
 		return email;
 	}
 
-	@Override
 	public Vehicle getRentedVehicle() {
 		return rentedVehicle;
 	}
 
-	@Override
 	public void setRentedVehicle(Vehicle rentedVehicle) {
 		this.rentedVehicle = rentedVehicle;
 	}
 
-	/* (non-Javadoc)
-	 * @see entity.Reservation#setEmail(java.lang.String)
-	 */
-	@Override
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-	/* (non-Javadoc)
-	 * @see entity.Reservation#getPickUp()
-	 */
-	@Override
 	public DateTime getPickUp() {
 		return pickUp;
 	}
 
-	/* (non-Javadoc)
-	 * @see entity.Reservation#setPickUp(org.joda.time.DateTime)
-	 */
-	@Override
 	public void setPickUp(DateTime pickUp) {
 		this.pickUp = pickUp;
 	}
 
-	/* (non-Javadoc)
-	 * @see entity.Reservation#getReturnTime()
-	 */
-	@Override
 	public DateTime getReturnTime() {
 		return returnTime;
 	}
 
-	/* (non-Javadoc)
-	 * @see entity.Reservation#setReturnTime(org.joda.time.DateTime)
-	 */
-	@Override
 	public void setReturnTime(DateTime returnTime) {
 		this.returnTime = returnTime;
 	}
 
-	/* (non-Javadoc)
-	 * @see entity.Reservation#getPrice()
-	 */
-	@Override
 	public BigDecimal getPrice() {
 		return price;
 	}
 
-	/* (non-Javadoc)
-	 * @see entity.Reservation#setPrice(java.math.BigDecimal)
-	 */
-	@Override
 	public void setPrice(BigDecimal price) {
 		this.price = price;
 	}
